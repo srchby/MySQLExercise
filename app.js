90ef75e47979fd9142336eaa41f8280b2aaea82d
searchby <raidentsuji@gmail.com>
2024-02-02 18:37:46 -0300

    separating directories

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');  
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//create
+app.post('/create', (req, res) => {})
+
+//read
+app.get('/getAll', (request, response) => {
+    console.log('Test');
+})
+
+app.listen(process.env.PORT, () => console.log('app is running'));
