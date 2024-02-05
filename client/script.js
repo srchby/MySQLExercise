6a245c32cb1d4c829ea7065e59488fa3465d8e12
searchby <raidentsuji@gmail.com>
2024-02-05 17:54:08 -0300

    dbservices.js

--- client/script.js
+++ client/script.js
@@ -5,11 +5,11 @@ document.addEventListener("DOMContentLoaded", function () {
     loadtHTMLTable([])
 })
 
-function loadtHTMLTable(data) {
-    const table = document.querySelector("table tbody")
+function loadHTMLTable(data) {
+    const table = document.querySelector('table tbody');
 
     if (data.length === 0) {
-        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>"
-        return
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+        return;
     }
 }
