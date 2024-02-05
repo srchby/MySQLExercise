ebaf4ca8d188a32fa6474dc01a0e40a5f5645f06
searchby <raidentsuji@gmail.com>
2024-02-05 19:38:12 -0300

    end of day

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch('http://localhost:5000/getAll')
     .then(response => response.json())
     .then(data => console.log(data))
-    loadtHTMLTable([])
+    loadHTMLTable([]);
 })
 
 function loadHTMLTable(data) {
